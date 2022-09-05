4428c90ba91b3105b728e8919acc845f63ffb939
Ayslan Fernandes <lalanfernandes@gmail.com>
2022-09-05 12:35:46 -0300

    configs eslint

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const numero = 10;
+/* const numero = 10;
 console.info('numero:' + numero);
 
 const variavelNumerica = 2;
@@ -14,4 +14,8 @@ if (
     && variavelNumerica == true
 ) {
     console.info('data');
-}
+} */
+
+const variavelNumerica = '2';
+
+if (variavelNumerica == 2 && variavelNumerica === 2) console.log(`data:${variavelNumerica}`);
