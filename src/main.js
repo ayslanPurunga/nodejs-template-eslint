2a7ba3c152762f9c862bfc0abe36a48f4f04fe77
Ayslan Fernandes <lalanfernandes@gmail.com>
2022-09-05 12:06:27 -0300

    changes archives e prettier configs

--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
 const numero = 10;
-console.info("numero:" + numero);
+console.info('numero:' + numero);
+
+const variavelNumerica = 2;
+
+if (
+    variavelNumerica == true
+    && variavelNumerica == true
+    && variavelNumerica == true
+    && variavelNumerica == true
+    && variavelNumerica == true
+    && variavelNumerica == true
+    && variavelNumerica == true
+    && variavelNumerica == true
+) {
+    console.info('data');
+}
