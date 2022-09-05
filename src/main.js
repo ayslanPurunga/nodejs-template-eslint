c4888314e1411821ad100371f70b7ee9146c4717
Ayslan Fernandes <lalanfernandes@gmail.com>
2022-09-05 12:53:33 -0300

    Update main.js VSCode

--- src/main.js
+++ src/main.js
@@ -19,6 +19,6 @@ if (
 const variavelNumerica = '2';
 
 if (variavelNumerica === 2 && variavelNumerica === 2) {
-    console.info('data 22');
+    console.info('data xyz');
     console.info(`data:${variavelNumerica}`);
 }
